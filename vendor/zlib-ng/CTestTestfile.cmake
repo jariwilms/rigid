35cc33b2f90a2c8fc45b9a87c0636a98cee84aa0
Jari <jariwilms@outlook.com>
2025-08-19 07:47:19 +0200

    removed redundant files

--- vendor/zlib-ng/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jariw/Desktop/zlib-ng-2.2.5
-# Build directory: C:/Users/jariw/Desktop/zlib-ng-2.2.5
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
